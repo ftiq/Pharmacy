4bc4e9c2a74ec0ea81f7139864fa26de6bf07d72
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-07 23:54:12 +0300

    Update pos_store.js

--- sh_pos_all_in_one_retail/sh_pos_all_in_one_retail/static/global_models/static/src/override/pos_store.js
+++ sh_pos_all_in_one_retail/sh_pos_all_in_one_retail/static/global_models/static/src/override/pos_store.js
@@ -46,7 +46,7 @@ patch(PosStore.prototype, {
         var order = this.get_order()
         var self = this;
         console.log("outer")
-        if (this.config.sh_show_qty_location && this.config.sh_display_stock && product.type == "product") {
+        if (true) { //this.config.sh_show_qty_location && this.config.sh_display_stock && product.type == "product") {
             console.log("11111")
             var sh_min_qty = this.config.sh_min_qty
             var location_id = this.config.sh_pos_location ? this.config.sh_pos_location[0] : false
