bef42057533bf7cea951fc3ebf6e5139997be517
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-08 01:33:28 +0300

    Update pos_store.js

--- sh_pos_all_in_one_retail/sh_pos_all_in_one_retail/static/sh_pos_wh_stock/static/src/app/overrides/models/pos_store.js
+++ sh_pos_all_in_one_retail/sh_pos_all_in_one_retail/static/sh_pos_wh_stock/static/src/app/overrides/models/pos_store.js
@@ -9,6 +9,7 @@ import { _t } from "@web/core/l10n/translation";
 
 patch(PosStore.prototype, {
     async _processData(loadedData) {
+        console.log({loadedData})
         await super._processData(...arguments);
         if (loadedData['quant_by_product_id']) {
             this.db.quant_by_product_id = loadedData['quant_by_product_id']
